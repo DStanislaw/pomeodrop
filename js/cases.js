8ad063249fd3eac5446c0fda4964f17d68f6d880
DStanislaw <123885886+Ponurr@users.noreply.github.com>
2025-01-19 00:55:01 +0100

    narazie dziala w miare git ale duzo poprawek potrzeba

---
+++ js/cases.js
@@ -0,0 +1,32 @@
+  const boxes = document.getElementById('boxes');
+        const spinButton = document.getElementById('spinButton');
+        const boxWidth = 300; 
+        const totalBoxes = 150;
+        const visibleBoxes = 5;
+        const spinDuration = 7000; 
+
+        function spin() {
+            
+            spinButton.disabled = true;
+
+           
+            const winningIndex = Math.floor(Math.random() * totalBoxes);
+
+            
+            const offset = (winningIndex - Math.floor(visibleBoxes / 2)) * boxWidth;
+
+
+            boxes.style.transition = `left ${spinDuration}ms cubic-bezier(0.33, 1, 0.68, 1)`;
+            boxes.style.left = `-${offset}px`;
+
+            
+            setTimeout(() => {
+                alert(`Wygrana: Box #${winningIndex + 1}`);
+               
+                boxes.style.transition = 'none';
+                boxes.style.left = '0px';
+                spinButton.disabled = false;
+            }, spinDuration);
+        }
+
+        spinButton.addEventListener('click', spin);
